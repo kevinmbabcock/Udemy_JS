ec928dc09677de74c5d6caf7d7c3abda2c772c9a
Kevin M Babcock <kevinmbabcock@icloud.com>
2018-11-02 22:06:28 -0700

    variables and data types

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,80 @@
+/***
+* Variables and data types
+*/
+
+
+// var firstName = 'John';
+// console.log(firstName);
+// 
+// var lastName = 'Smith';
+// var age = 28;
+// 
+// var fullAge = true;
+// console.log(fullAge);
+// 
+// var job;
+// console.log(job);
+// 
+// job = 'Teacher';
+// console.log(job);
+// 
+
+
+//  Variable naming rules
+// var _3years = 3;
+// var johnMark = 'John and Mark';
+// var if = 23;
+ 
+
+/***
+* Variable mutation and type coercion
+*/
+
+// var firstName = 'John';
+// var age = 28;
+// 
+// console.log(firstName + ' ' + age);
+// 
+// var job, isMarried;
+// job = 'Teacher';
+// isMarried = false;
+// 
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+
+// age = 'twenty eight';
+// job = 'driver';
+// 
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// 
+// var lastName = prompt('What is his last Name?');
+// console.log(firstName + ' ' +  lastName);
+
+// Basic Operators
+
+// var year, yearJohn, yearMark
+// now = 2018;
+// ageJohn = now - 28;
+// ageMark = now - 33;
+
+// Math operators
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
+// 
+// console.log(yearJohn);
+// 
+// console.log(now + 2);
+// console.log(now * 2);
+// console.log(now / 10);
+
+// Logical Operators
+// var johnOlder = ageJohn > ageMark;
+// console.log(johnOlder);
+
+// type of operator
+// console.log(typeof johnOlder);
+// console.log(typeof ageJohn);
+// console.log(typeof 'Mark is older than John');
+// var x;
+// console.log(typeof x);
